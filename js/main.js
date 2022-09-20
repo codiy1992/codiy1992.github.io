182896e51cc6136d5e42b1fd7427cdee174e84bd
codiy <mail@codiy.net>
2022-09-20 23:13:05 +0800

    Updated at 2022-09-20T23:13:05+08:00

--- js/main.js
+++ js/main.js
@@ -169,6 +169,24 @@ limitations under the License.
 ;
 
 
+
+
+(function($) {
+    var needMarkmap = false;
+    $('.language-markmap').parent().replaceWith(function() {
+        needMarkmap = true;
+        return $('<div class="markmap">').text($(this).text());
+    });
+
+    const { markmap } = window;
+    if(needMarkmap) {
+        markmap.autoLoader.renderAll();
+    }
+
+})(jQuery);
+
+
+
 ;
 
 
